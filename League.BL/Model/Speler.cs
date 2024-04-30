3a8b62a35f7f2d84afd3cd55b1f3585e08100c2c
Tom Vande Wiele <tvdewiel@hotmail.com>
2024-04-30 09:46:18 +0200

    Add project files.

---
+++ League.BL/Model/Speler.cs
@@ -0,0 +1,71 @@
+﻿using League.BL.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace League.BL.Model
+{
+    public class Speler
+    {
+        internal Speler(string naam, int? lengte, int? gewicht) 
+        {
+            ZetNaam(naam);
+            if (lengte!=null) ZetLengte(lengte.Value);
+            if (gewicht.HasValue) ZetGewicht(gewicht.Value);
+        }
+
+        internal Speler(int id, string naam, int? lengte, int? gewicht) :this(naam,lengte,gewicht)
+        {
+            ZetId(id);
+        }
+
+        public int Id { get; private set; }
+        public string Naam { get; private set; }
+        public int? Rugnummer { get; private set; }
+        public int? Lengte { get; private set; }
+        public int? Gewicht { get; private set; }
+        public Team Team { get; private set; }
+        public void ZetId(int id)
+        {
+            if (id <= 0) throw new SpelerException("ZetId");
+            Id=id;
+        }
+        public void ZetNaam(string naam)
+        {
+            if (string.IsNullOrEmpty(naam)) { throw new SpelerException("ZetNaam"); }
+            Naam = naam;
+        }
+        public void ZetLengte(int lengte)
+        {
+            if (lengte < 150) throw new SpelerException("ZetLengte");
+            Lengte = lengte;
+        }
+        public void ZetGewicht(int gewicht)
+        {
+            if (gewicht < 50) throw new SpelerException("ZetGewicht");
+            Gewicht=gewicht;
+        }
+        public void ZetRugnummer(int rugnummer)
+        {
+            if ((rugnummer <= 0) || (rugnummer > 99)) throw new SpelerException("ZetRugnummer");
+            Rugnummer = rugnummer;
+        }
+        public void ZetTeam(Team team)
+        {
+            //TODO zetteam
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Speler speler &&
+                   Id == speler.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
---
+++ League.BL/Model/Team.cs
@@ -0,0 +1,54 @@
+﻿using League.BL.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace League.BL.Model
+{
+    public class Team
+    {
+        public int Stamnummer {  get;private set; }
+        public string Naam {  get; private set; }
+        public string Bijnaam { get; private set; }
+        private List<Speler> _spelers = new List<Speler>();
+        
+        internal Team(int stamnummer, string naam)
+        {
+            ZetStamnummer(stamnummer);
+            ZetNaam(naam);
+        }
+
+        public void ZetStamnummer(int stamnummer)
+        {
+            if (stamnummer <= 0) throw new TeamException("ZetTeam");
+            Stamnummer = stamnummer;
+        }
+        public void ZetNaam(string naam)
+        {
+            if (string.IsNullOrWhiteSpace(naam)) throw new TeamException("ZetNaam");
+            Naam = naam;
+        }
+        public void ZetBijnaam(string naam)
+        {
+            if (string.IsNullOrWhiteSpace(naam)) throw new TeamException("ZetBijnaam");
+            Bijnaam = naam;
+        }
+        public IReadOnlyList<Speler> Spelers()
+        {
+            return _spelers.AsReadOnly();
+        }
+        internal void VoegSpelerToe(Speler speler)
+        {
+            if (speler==null) throw new SpelerException("VoegSpelerToe");
+            if (_spelers.Contains(speler)) throw new SpelerException("VoegSpelerToe");
+            _spelers.Add(speler);
+
+        }
+        internal void VerwijderSpeler(Speler speler)
+        {
+
+        }
+    }
+}
